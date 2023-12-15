3a65de3c2fc7fd5652b41cb3c791157223d37639
MaximeRichard78 <maxime.richard@ecole-hexagone.com>
2023-12-15 12:54:37 +0100

    debut base de donnée

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -2,34 +2,27 @@
 
 namespace App\Controller;
 
-use App\Entity\Work;
-use App\Form\WorkType;
+use App\Entity\Training;
+use App\Form\TrainingType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WorkController extends AbstractController
+class TrainingController extends AbstractController
 {
-    #[Route('/work/create', name: 'work_create')]
+    #[Route('/training/create', name: 'training_create')]
     public function create(Request $request): Response
     {
-        $work = new Work();
+        $training = new Training();
 
-        $form = $this->createForm(WorkType::class, $work);
+        $form = $this->createForm(TrainingType::class, $training);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dump($work);
+            dump($training);
         }
-        return $this->render('work/form.html.twig', [
+        return $this->render('training/form.html.twig', [
             'form' => $form->createView(),
-        ]);
-    }
-
-    #[Route('/works', name: 'works')]
-    public function works(): Response
-    {
-        return $this->render('work/works.html.twig', [
         ]);
     }
 }
---
+++ src/Entity/Work.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WorkRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WorkRepository::class)]
+class Work
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $startDate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $endDate = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: '', targetEntity:Training ::class)]
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of startDate
+     */
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * Set the value of startDate
+     */
+    public function setStartDate(?\DateTimeInterface $startDate): self
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of endDate
+     */
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * Set the value of endDate
+     */
+    public function setEndDate(?\DateTimeInterface $endDate): self
+    {
+        $this->endDate = $endDate;
+
+        return $this;
+    }
+}
